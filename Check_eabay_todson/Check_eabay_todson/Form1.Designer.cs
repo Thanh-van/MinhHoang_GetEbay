6ae36a7658681ed6fa7383e7363c6a51772071f6
Bùi Thanh Văn <49279236+Thanh-van@users.noreply.github.com>
2020-09-09 11:26:24 +0700

    up

---
+++ Check_eabay_todson/Check_eabay_todson/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtlogebay = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLogHistory = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtlogebay
+            // 
+            this.txtlogebay.Location = new System.Drawing.Point(529, 153);
+            this.txtlogebay.Name = "txtlogebay";
+            this.txtlogebay.Size = new System.Drawing.Size(390, 400);
+            this.txtlogebay.TabIndex = 9;
+            this.txtlogebay.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLogHistory
+            // 
+            this.txtLogHistory.Location = new System.Drawing.Point(56, 153);
+            this.txtLogHistory.Name = "txtLogHistory";
+            this.txtLogHistory.Size = new System.Drawing.Size(390, 400);
+            this.txtLogHistory.TabIndex = 7;
+            this.txtLogHistory.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 621);
+            this.Controls.Add(this.txtlogebay);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLogHistory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtlogebay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox txtLogHistory;
+    }
+}
+
---
+++ Check_eabay_todson/Check_eabay_todson/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //keywordSearch = txtKeyword.Text.Trim();
+            Thread newThread = new Thread(DoWork);
+            newThread.Start();
+
+        }
+        /// <summary>
+        /// Load link-product ebay 
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        public void LoadPageEbayByKeyword(string keyword)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            //client.Headers["User-Agent"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Mobile/15E148 Snapchat/10.77.5.59 (like Safari/604.1)";
+            string htmlRequest = client.DownloadString($"https://www.ebay.com/sch/i.html?_nkw={keyword}");
+            if (true)
+            {
+
+            }
+
+
+            MatchCollection mcLinkProduct = Regex.Matches(htmlRequest, "<a (.+?) class=s-item__link href=(.+?)>(.+?)</a>");
+            if (mcLinkProduct.Count != 0)
+            {
+                foreach (Match item in mcLinkProduct)
+                {
+                    //linkProductModel.NameProduct = item.Groups[2].Value.Trim();
+                    String link_product = item.Groups[2].Value.Trim();
+                    //WriteLogHistory(link_product, Color.Green);
+                    LoadLink(link_product);
+                    break;
+                }
+            }
+
+        }
+        public String LoadLink(string link)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            //client.Headers["User-Agent"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Mobile/15E148 Snapchat/10.77.5.59 (like Safari/604.1)";
+            string htmlRequest = client.DownloadString($"{link}");
+            //WriteLogSolid(htmlRequest, Color.Green);
+            if (true)
+            {
+
+            }
+
+            MatchCollection mcLinksolod = Regex.Matches(htmlRequest, "<a class=\"vi-txt-underline\" href=(.+?)>(.+?)</a>");
+            if (mcLinksolod.Count != 0)
+            {
+                MatchCollection mcLinkProduct = Regex.Matches(htmlRequest, "<h1 (.+?)>(.+?)</span>(.+?)</h1>");
+                foreach (Match item in mcLinkProduct)
+                {
+                    String name = item.Groups[3].Value.Trim();
+                    if (name == null)
+                        name = "Thanh Van";
+                    WriteLogSolid(link, Color.Red);
+                    WriteLogSolid(name, Color.Green);
+
+                    break;
+                }
+                foreach (Match item in mcLinksolod)
+                {
+
+                    String soild = item.Groups[2].Value.Trim();
+                    WriteLogSolid(soild, Color.Green);
+                    //WriteLogSolid(link, Color.Red);
+                    break;
+                }
+
+            }
+            //else
+            //{
+            //	WriteLogSolid("Thanh Van", Color.Red);
+            //}
+
+            return "";
+        }
+        private void WriteLogSolid(string logText, Color status)
+        {
+            //_busy.WaitOne();
+            try
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    txtlogebay.Select(0, 0);
+                    txtlogebay.SelectionColor = status;
+                    txtlogebay.SelectedText = DateTime.Now.ToString("HH:mm:ss") + " : " + logText + "\r\n";
+                    //var lines = txtLogHistory.Lines;
+                    //txtLogHistory.Lines = lines.Skip(lines.Length - 20).ToArray();
+                    return;
+                }));
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+        /// <summary>
+        /// Load name product link
+        /// </summary>
+        /// 
+        public bool NameProduct(string htmlRequest)
+        {
+            MatchCollection mcLinkProduct = Regex.Matches(htmlRequest, "<h5><a(.+?)>(.+?)</a></h5>");
+            if (mcLinkProduct.Count > 1)
+            {
+                foreach (Match item in mcLinkProduct)
+                {
+                    String dl = item.Groups[2].Value.Trim();
+                    if (dl != "Tags")
+                    {
+                        WriteLogHistory(dl, Color.Green);
+                        LoadPageEbayByKeyword(dl);
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Loadtdson()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            List<String> str = new List<String>();
+            str.Add("velox");
+            str.Add("onguard");
+            str.Add("elite");
+            str.Add("topeak");
+            foreach (var item in str)
+            {
+                int x = 0, i = 0;
+                WriteLogHistory("------------------Đầu "+item+"----------------------------", Color.Red);
+                while (x != 1)
+                    while (x != 1)
+                {
+                    i++;
+                    client.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36";
+                    string velox = client.DownloadString($"https://www.todson.com/collections/{item}?page={i}");
+                    if (NameProduct(velox))
+                            NameProduct(velox);
+                    else
+                        x = 1;
+                }
+                WriteLogHistory("------------------Hết " + item + "----------------------------", Color.Red);
+            }
+            
+        }
+
+        private void WriteLogHistory(string logText, Color status)
+        {
+            //_busy.WaitOne();
+            try
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    txtLogHistory.Select(0, 0);
+                    txtLogHistory.SelectionColor = status;
+                    txtLogHistory.SelectedText = DateTime.Now.ToString("HH:mm:ss") + " : " + logText + "\r\n";
+                    return;
+                }));
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+        private void WriteLogEbay(string logText, Color status)
+        {
+            //_busy.WaitOne();
+            try
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    txtlogebay.Select(0, 0);
+                    txtlogebay.SelectionColor = status;
+                    txtlogebay.SelectedText = DateTime.Now.ToString("HH:mm:ss") + " : " + logText + "\r\n";
+                    return;
+                }));
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+        public void DoWork()
+        {
+            Loadtdson();
+
+        }
+    }
+}
